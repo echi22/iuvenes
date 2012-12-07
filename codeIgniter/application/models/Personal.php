d1cf445d54e1469487943c579fc67f6c48f125c8
echi22 <echi22@a0193c38-b79d-4e96-b3a6-a659de7f0f6b>
2012-12-07 18:15:37 +0000

    personal

---
+++ codeIgniter/application/models/Personal.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of alumno_model
+ *
+ * @author echi
+ */
+class Personal extends DataMapper {
+    var $table = 'personal';
+    var $has_one = array("persona");
+    
+    function __construct($id = NULL)
+    {
+        parent::__construct($id);
+    }
+}
+
+?>
